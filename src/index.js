4620e4da611ae0b42115db86fedc7e92c95549a4
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-07-19 10:32:41 -0500

    npm exercise

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const $ = require('jquery');
+const sayHello = () => console.log("Hello"); {
+ sayHello()
+}
+
+
+$('body').css('background-color', 'papayawhip');
+const yoMamma = require('yo-mamma').default;
+let insult;
+
+// get random joke
+insult = yoMamma();
+
+
+//Yo mama is so old that she knew Burger King while he was still a prince.
+console.log(insult);
